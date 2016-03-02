5021f0435d9ae85a49bbb9c81b1909e7363151ce
sharonrussell <sharon.russell@comparethemarket.com>
2016-03-02 10:43:23 +0000

    further completing the Composite example

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -4,7 +4,7 @@ namespace Composite
 {
     public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {   
             var allVillagers = AllVillagers();
             allVillagers.InviteMyMostStupendousParty();
@@ -17,11 +17,51 @@ namespace Composite
             var maiya = new Meerkat("Maiya");
             var vassily = new Meerkat("Vassily");
             var yakov = new Meerkat("Yakov");
-            var bogdan = new Meerkat("Bogdan");
 
+            var sergeiFamily = CreateSergeiFamily();
+            var bogdanFamily = CreateBogdanFamily();
+
+            var allVillagers = new Mob
+            {
+                Name = "All villagers",
+                Members = {maiya, vassily, yakov, sergeiFamily, bogdanFamily}
+            };
+
+            return allVillagers;
+        }
+
+        private static Mob CreateBogdanFamily()
+        {
+            var bogdan = new Meerkat("Bogdan");
+            var mothermabobOfBogdan = new Meerkat("Mothermabob of Bogdan");
+            var fathermabobOfBogdan = new Meerkat("Fathermabob of Bogdan");
+
+            var bogdanFamily = new Mob
+            {
+                Name = "family of Bogdan",
+                Members = {bogdan, mothermabobOfBogdan, fathermabobOfBogdan}
+            };
+
+            return bogdanFamily;
+        }
+
+        private static Mob CreateSergeiFamily()
+        {
             var sergei = new Meerkat("Sergei");
             var mostBeautiousSisterofSergei = new Meerkat("Beautious Sister Of Sergei");
+            var familyMeThinksHasFleas = CreateFamilyMeThinksHasFleas();
 
+            var sergeiFamily = new Mob
+            {
+                Name = "family of Sergei",
+                Members = {sergei, mostBeautiousSisterofSergei, familyMeThinksHasFleas}
+            };
+
+            return sergeiFamily;
+        }
+
+        private static Mob CreateFamilyMeThinksHasFleas()
+        {
             var brotherOfSergeiAlwaysWithTheScratching = new Meerkat("Scratching Brother of Sergei");
             var wifeOfScratchingBrother = new Meerkat("Wife of Scratching Brother");
             var scratchyBaby = new Meerkat("Baby of Scratching Brother");
@@ -32,20 +72,7 @@ namespace Composite
                 Members = {brotherOfSergeiAlwaysWithTheScratching, wifeOfScratchingBrother, scratchyBaby}
             };
 
-            var sergeiFamily = new Mob {Name = "family of Sergei", Members = {sergei, mostBeautiousSisterofSergei}};
-
-            var mothermabobOfBogdan = new Meerkat("Mothermabob of Bogdan");
-            var fathermabobOfBogdan = new Meerkat("Fathermabob of Bogdan");
-            var bogdanFamily = new Mob {Name = "family of Bogdan", Members = {bogdan, mothermabobOfBogdan, fathermabobOfBogdan}};
-
-            var meerkovaVillageFamilies = new Mob
-            {
-                Name = "Meerkova village families",
-                Members = {sergeiFamily, bogdanFamily, familyMeThinksHasFleas}
-            };
-
-            var allVillagers = new Mob {Name = "All villagers", Members = {maiya, vassily, yakov, meerkovaVillageFamilies}};
-            return allVillagers;
+            return familyMeThinksHasFleas;
         }
     }
 }
